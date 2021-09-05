c15796aaa93589beacde90d54374dc9cb61e4db7
Jason Ziegler <jason@insightfulautomation.com>
2021-09-05 16:40:42 -0500

    create course api routes

--- app.js
+++ app.js
@@ -23,16 +23,34 @@ app.get('/', (req, res) => {
 
 // A /api/users GET route that will return all properties and values for the currently authenticated User along with a 200 HTTP status code.
 app.get('/api/users', (async (req, res) => {
-  res.json({
-    message: 'This route should return a user',
-  });
+  res.status(200).json({ "message": "This GET route should return user information for the currently authenticated User" });
 }));
 
 // A /api/users POST route that will create a new user, set the Location header to "/", and return a 201 HTTP status code and no content.
 app.post('/api/users', (async (req, res) => {
-  res.status(201).json({ "message": "This post route should create a user and return: Account successfully created!" });
+  res.status(201).json({ "message": "This POST route should create a user and return no content" });
 }));
 
+// A /api/courses GET route that will return all courses including the User associated with each course and a 200 HTTP status code.
+app.get('/api/courses', (async (req, res) => {
+  res.status(200).json({ "message": "This GET route should return all courses and User Associateed" });
+}));
+// A /api/courses/:id GET route that will return the corresponding course including the User associated with that course and a 200 HTTP status code.
+app.get('/api/courses/:id', (async (req, res) => {
+  res.status(200).json({ "message": "This GET route should return the course with the associated id" });
+}));
+// A /api/courses POST route that will create a new course, set the Location header to the URI for the newly created course, and return a 201 HTTP status code and no content.
+app.post('/api/courses', (async (req, res) => {
+  res.status(201).json({ "message": "This POST route should create a course and return no content" });
+}));
+// A /api/courses/:id PUT route that will update the corresponding course and return a 204 HTTP status code and no content.
+app.put('/api/courses/:id', (async (req, res) => {
+  res.status(204).json({ "message": "This PUT route should UPDATE a course and return no content" });
+}));
+// A /api/courses/:id DELETE route that will delete the corresponding course and return a 204 HTTP status code and no content.
+app.delete('/api/courses/:id', (async (req, res) => {
+  res.status(204).json({ "message": "This DELETE route should DELETE a course and return no content" });
+}));
 
 // send 404 if no other route matched
 app.use((req, res) => {
