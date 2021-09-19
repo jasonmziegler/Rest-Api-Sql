cb2cf7151bd8bac6b3bc97f9a333d83df60f8d1d
Jason Ziegler <jason@insightfulautomation.com>
2021-09-19 14:33:20 -0500

    added validation to user model

--- models/user.js
+++ models/user.js
@@ -9,18 +9,62 @@ module.exports = (sequelize) => {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
-        },
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'An id is require to create a user record.'
+                }
+            }
+        }     
+        ,
         firstName: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A First Name is required to create a user record.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a first name.'
+                }
+            }
+            
         },
         lastName: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A Last Name is required to create a user record.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a first name.'
+                }
+            }
+            
         },
         emailAddress: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A Email Address is required to create a user record.'
+                },
+                isEmail: {
+                    msg: 'Please provide a valid email address.'
+                }
+            }
+            
         },
         password: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A Password is required to create a user record.'
+                }
+            }
+            
         }
     }, { sequelize });
 
